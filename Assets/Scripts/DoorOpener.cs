e4066a0383b64a7e706f5c38e5d47d652ffca98e
nyyppa <atte.yliverronen@gmail.com>
2016-11-01 20:26:02 +0200

    +DoorOpener -door stays open

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour {
+	public float motorForce = 100;
+	public float MotorRunningTime=1;
+	float timeMotorHasRunFor=2;
+	HingeJoint joint;
+	JointMotor orginal;
+	bool orginalMotor=true;
+	// Use this for initialization
+	void Start () {
+		joint = GetComponentInParent<HingeJoint> ();
+		orginal = joint.motor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeMotorHasRunFor += Time.deltaTime;
+		if(timeMotorHasRunFor>MotorRunningTime){
+			if(!orginalMotor){
+				orginalMotor = true;
+				joint.motor = orginal;
+			}
+		}
+	}
+
+	public void Open(int direction){
+		if(timeMotorHasRunFor>MotorRunningTime){
+			JointMotor motor = new JointMotor ();
+			motor.force = motorForce*direction;
+			joint.motor = motor;
+			orginalMotor = false;
+		}
+	}
+}
---
+++ Assets/Scripts/UseMotorDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseMotorDoor : MonoBehaviour {
+	public int direction=1;
+	DoorOpener opener;
+	// Use this for initialization
+	void Start () {
+		opener = GetComponentInParent<DoorOpener> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter(Collider collider){
+		if(collider.tag.Equals("Guard")){
+			opener.Open (direction);
+		}
+	}
+	void OnTriggerStay(Collider collider){
+		if(collider.tag.Equals("Guard")){
+			opener.Open (direction);
+		}
+	}
+}
