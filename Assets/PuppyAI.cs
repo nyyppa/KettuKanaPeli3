72b2aa412bc28889a3aae612383a8a9d576750e7
Veikka Saaristo <veikka.saaristo@hotmail.com>
2016-11-08 13:13:33 +0200

    + Puppy added + Puppy color corrected + Puppy animations implemented * Puppy running not yet in code * Puppy and chicken shader outlines ridiculously thick

---
+++ Assets/PuppyAI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuppyAI : MonoBehaviour {
+    Animator _animator;
+    
+	void Start () {
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update () {
+	
+	}
+
+    void Walk() {
+        _animator.SetBool("isRunning", true);
+    }
+}
